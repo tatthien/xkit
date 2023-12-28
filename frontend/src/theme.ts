99d2d744f6769fc77ac1cafacfda97f71a9fd151
Thien Nguyen <tatthien.contact@gmail.com>
2023-12-29 02:06:57 +0700

    chore: change TextInput height

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,8 +1,24 @@
-import { Button, createTheme, rem } from "@mantine/core";
+import { Button, createTheme, Input, rem, TextInput } from "@mantine/core";
 
 export const theme = createTheme({
   primaryColor: "dark",
   components: {
+    TextInput: TextInput.extend({
+      defaultProps: {
+        size: "sm",
+      },
+      vars: (theme, props) => {
+        if (props.size === "sm") {
+          return {
+            wrapper: {
+              "--input-height": rem(34),
+            },
+          };
+        }
+
+        return { wrapper: {} };
+      },
+    }),
     Button: Button.extend({
       defaultProps: {
         size: "sm",
